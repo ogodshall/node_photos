0332bfa4c68831e36043b77abeea2a5c4a8a1002
O. Godshall <ogodshall@gmail.com>
2017-03-18 20:34:20 -0400

    Added POST functionality to load_albums

--- load_albums.js
+++ load_albums.js
@@ -79,16 +79,25 @@ function load_album (album_name, page, page_size, callback) {
 	});
 }
 
+function do_rename(old_name, new_name, callback) {
+	// renames the album folder
+	console.log("Renaming folder: " + old_name);
+	fs.rename("albums/" + old_name,
+			  "albums/" + new_name,
+			  callback);
+	console.log("Folder has been renamed: " + new_name);
+}
+
 function handle_incoming_request (req, res) {
-	console.log(url.parse(req.url, true));
 	req.parsed_url = url.parse(req.url, true);
 	var core_url = req.parsed_url.pathname;
-	
+
 	console.log("INCOMING REQUEST: " + req.method + " " + req.url);
 	if (core_url == '/albums.json') {
 		handle_list_albums(req, res);
-	} else if (core_url.substr(0, 7) == '/albums' 
-			   && core_url.substr(core_url.length - 5) == '.json') {
+	} else if (core_url.substr(core_url.length - 12) == '/rename.json' && req.method.toLowerCase() == 'post') {
+		handle_rename_album(req, res);
+	} else if (core_url.substr(0, 7) == '/albums' && core_url.substr(core_url.length - 5) == '.json') {
 		handle_get_album(req, res);
 	} else {
 		send_failure(res, 404, invalid_resource());
@@ -110,8 +119,8 @@ function handle_list_albums(req, res) {
 function handle_get_album(req, res) {
 	// get the GET params
 	var getp = req.parsed_url.query;
-	var page_num = getp.page ? getp.page : 0;
-	var page_size = getp.page_size ? getp.page_size : 1000;
+	var page_num = getp.page ? parseInt(getp.page) : 0;
+	var page_size = getp.page_size ? parseInt(getp.page_size) : 1000;
 
 	if (isNaN(parseInt(page_num))) page_num = 0;
 	if (isNaN(parseInt(page_size))) page_size = 1000;
@@ -131,6 +140,67 @@ function handle_get_album(req, res) {
 	});
 }
 
+function handle_rename_album (req, res) {
+	// gets the album name from the url
+	var core_url = req.parsed_url.pathname;
+	var parts = core_url.split('/');
+	if (parts.length != 4) {
+		send_failure(res, 404, invalid_resource());
+		return;
+	}
+
+	var album_name = parts[2];
+
+	// gets the POST data for the request
+	// includes the JSON name for the new album
+	var json_body = '';
+	req.on('readable', function() {
+		var d = req.read();
+		if (d) {
+			if (typeof d == 'string') {
+				json_body += d;
+			} else if (typeof d == 'object' && d instanceof Buffer) {
+				json_body += d.toString('utf8');
+			}
+		}
+	});
+
+	// Validate the POST data and then attempt rename
+	req.on('end', function() {
+		// checks for json_body
+		if (json_body) {
+			try {
+				var album_data = JSON.parse(json_body);
+				if (!album_data.album_name) {
+					send_failure(res, 404, missing_data('album_name'));
+					return;
+				}
+			} catch (e) {
+				// json_body exists, but is bad json
+				console.log("BODY IS BAD");
+				send_failure(res, 403, bad_json());
+				return;
+			}
+			// perform the rename
+			console.log("RENAMING ENGAGED");
+			do_rename(album_name, album_data.album_name, function (err, results) {
+				if (err && err.code == "ENOENT") {
+					send_failure(res, 403, no_such_album());
+					return;
+				} else if (err) {
+					send_failure(res, 500, file_error(err));
+					return;
+				}
+				send_success(res, null);
+			});
+		} else { // didn't get json_body
+			console.log("NO BODY");
+			send_failure(res, 403, bad_json());
+			res.end();
+		}
+	});
+}
+
 function make_error (err, msg) {
 	var e = new Error(msg);
 	e.code = err;
@@ -143,9 +213,9 @@ function send_success (res, data) {
 	res.end(JSON.stringify(output) + "\n");
 }
 
-function send_failure (res, code, err) {
+function send_failure (res, server_code, err) {
 	var code = (err.code) ? err.code : err.name;
-	res.writeHead(code, { "Content-Type" : "application/json" });
+	res.writeHead(server_code, { "Content-Type" : "application/json" });
 	res.end(JSON.stringify({ error: code, message: err.message }) + "\n");
 }
 
@@ -155,6 +225,14 @@ function invalid_resource() {
 
 function no_such_album() {
 	return make_error("no such album", "the specified album does not exist");
+}
+
+function bad_json() {
+	return make_error("invalid json", "the provided data is not valid JSON");
+}
+
+function missing_data() {
+	return make_error("missing data", "some data is missing!");
 }
 
 var s = http.createServer(handle_incoming_request);
